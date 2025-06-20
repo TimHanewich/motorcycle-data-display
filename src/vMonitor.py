061c1e2b2cda765a97523a54c5203dea3d97f5c9
Tim Hanewich <tahanewich@live.com>
2025-06-20 08:50:43 -0400

    Updated code to show voltage

--- src/vMonitor.py
+++ src/vMonitor.py
@@ -16,13 +16,29 @@ adc = machine.ADC(26)
 carry:int = adc.read_u16()
 alpha:float = 0.96
 
+def adc_to_supply_volts(adc_val:int) -> float:
+    """Converts the read ADC voltage to the supply voltage, based on this unique system, its current consumption, and test results gathered."""
+    m:float = 0.00025018479005
+    b:float = -0.04562527564001
+    return (m * adc_val) + b      # y = mx + b
+
 while True:
     
     val = adc.read_u16()
     carry = int((carry * alpha) + (val * (1 - alpha)))
+    volts:float = adc_to_supply_volts(carry)
+
+    # prepare line 1 (16 chars)
+    line1:str = str(val) + ", " + str(carry)
+    while len(line1) < 16:
+        line1 = line1 + " "
+
+    # prepare line 2 (16 chars)
+    line2:str = str(round(volts, 2)) + " volts"
+    while len(line2) < 16:
+        line2 = line2 + " "
     
     # show and wait
-    lcd.clear()
-    lcd.putstr(str(val) + ", " + str(carry))
+    lcd.putstr(line1 + line2)
     time.sleep(0.25)
     
